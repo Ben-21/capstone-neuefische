3b4e01f859fc795313638151cdfe9036c9f28cf7
Ben-21 <bs@schaefer-inet.de>
2023-07-24 14:29:50 +0200

    add deployment - test frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       <div>
+          This is my personal Test
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
