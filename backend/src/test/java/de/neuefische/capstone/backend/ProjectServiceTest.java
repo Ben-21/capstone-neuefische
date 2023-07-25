87e165e2d3c64ea9a98fe2ef5d6adba843419370
Ben-21 <bs@schaefer-inet.de>
2023-07-25 16:21:18 +0200

    add integrationtest

---
+++ backend/src/test/java/de/neuefische/capstone/backend/ProjectServiceTest.java
@@ -0,0 +1,62 @@
+package de.neuefische.capstone.backend;
+
+import de.neuefische.capstone.backend.models.Category;
+import de.neuefische.capstone.backend.models.Demand;
+import de.neuefische.capstone.backend.models.Project;
+import de.neuefische.capstone.backend.models.ProjectWithoutId;
+import de.neuefische.capstone.backend.services.IdService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import static org.springframework.aot.hint.TypeReference.listOf;
+
+class ProjectServiceTest {
+
+    ProjectRepo projectRepo = mock(ProjectRepo.class);
+    IdService idService = mock(IdService.class);
+    ProjectService projectService = new ProjectService(projectRepo, idService);
+
+
+    @Test
+    void whenProjectAdded_ThenReturnAddedProject() {
+        //Given
+        List<Demand> listOfDemands = new ArrayList<>(List.of(Demand.DONATIONINKIND));
+
+        Project projectWithId = new Project(
+                "01A",
+                "Earthquake Turky",
+                "Help for the people in Turky",
+                Category.PARTICIPATION,
+                listOfDemands,
+                50,
+                "Turky");
+
+
+        //When
+        when(idService.createRandomId())
+                .thenReturn("01A");
+        when(projectRepo.insert(projectWithId))
+                .thenReturn(projectWithId);
+
+
+        Project actualProject = projectService.addProject(new ProjectWithoutId(
+                "Earthquake Turky",
+                "Help for the people in Turky",
+                Category.PARTICIPATION,
+                listOfDemands,
+                50,
+                "Turky"));
+
+
+        //Then
+//        verify(projectRepo).insert(projectWithId);
+        verify(idService).createRandomId();
+        assertEquals(projectWithId, actualProject);
+    }
+}
