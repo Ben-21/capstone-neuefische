bcfaddefbac5201bc34728b5900e0e873c17dfc9
Ben-21 <bs@schaefer-inet.de>
2023-07-26 11:39:08 +0200

    add toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,16 @@ import {Link, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home.tsx";
 import AddProject from "./pages/AddProject.tsx";
 import styled from "styled-components";
+import {ToastContainer} from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 
 export default function App() {
 
     return (
 
         <>
+            <ToastContainer/>
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="/add-project" element={<AddProject/>}/>
--- frontend/src/hooks/useFetch.tsx
+++ frontend/src/hooks/useFetch.tsx
@@ -1,6 +1,7 @@
 import {Project} from "../models/models.tsx";
 import {create} from "zustand";
 import axios from "axios";
+import {toast} from 'react-toastify';
 
 
 type State = {
@@ -14,6 +15,8 @@ export const useFetch = create<State>(() => ({
     postProject: (requestBody: Project) => {
         axios
             .post("/api/projects", requestBody)
+            .then(() => toast.success("Project successfully added"))
             .catch(console.error)
+
     }
 }));
