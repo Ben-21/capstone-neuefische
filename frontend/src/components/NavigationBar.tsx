4949f8088ac26b25ec7cd01d442e3696fb4d7792
Ben-21 <bs@schaefer-inet.de>
2023-07-31 22:50:20 +0200

    add NavigationBar

---
+++ frontend/src/components/NavigationBar.tsx
@@ -0,0 +1,51 @@
+import AddButton from "./AddButton.tsx";
+import EditButton from "./EditButton.tsx";
+import styled from "@emotion/styled";
+import {useEffect, useState} from "react";
+import {useLocation} from "react-router-dom";
+import HomeButton from "./HomeButton.tsx";
+
+
+export default function NavigationBar() {
+    const location = useLocation();
+    const id = location.pathname.split("/")[2]
+    const [page, setPage] = useState("");
+
+    useEffect(() => {
+        if ((location.pathname.split("/")[1]) === "details") {
+            setPage("details")
+        } else {
+            setPage(location.pathname)
+        }
+    }, [location]);
+
+
+    return (
+        <StyledNavigationWrapper>
+            <StyledNavigationBar>
+                <HomeButton/>
+                {page === "/" && <AddButton/>}
+                {page === "details" && <EditButton projectId={id}/>}
+            </StyledNavigationBar>
+        </StyledNavigationWrapper>
+    )
+}
+
+const StyledNavigationBar = styled.div`
+  display: inline-flex;
+  border-radius: 5px 5px 0 0;
+  background-color: #888888;
+  height: 45px;
+`;
+
+const StyledNavigationWrapper = styled.div`
+  background-color: #fff;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  height: 60px;
+  display: flex;
+  justify-content: center;
+  align-items: end;
+`;
