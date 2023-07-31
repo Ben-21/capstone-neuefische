76f5b4d9fc139bca3d0c8cc6648a1866c2d5f5bf
Ben-21 <bs@schaefer-inet.de>
2023-07-31 22:50:08 +0200

    add buttons

---
+++ frontend/src/components/AddButton.tsx
@@ -0,0 +1,39 @@
+import {useNavigate} from "react-router-dom";
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import {IconButton} from "@mui/material";
+import styled from "@emotion/styled";
+
+export default function AddButton() {
+
+    const navigate = useNavigate();
+
+
+    function handleClick() {
+        navigate("/add-project")
+    }
+
+
+    return (
+        <StyledIconButton aria-label="Add" onClick={handleClick}>
+            <AddCircleIcon/>
+        </StyledIconButton>
+    )
+}
+
+const StyledIconButton = styled(IconButton)`
+  width: 46px;
+  height: 46px;
+
+  svg {
+    font-size: 32px;
+  }
+
+  border-radius: 5px;
+  border: none;
+  background-color: var(--colorBlack);
+  margin: 0;
+  padding: 0;
+  display: grid;
+  place-items: center;
+  cursor: pointer;
+`;
