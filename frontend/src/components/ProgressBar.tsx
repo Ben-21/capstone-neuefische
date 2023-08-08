2a393ba266228b8dd7b715455f4c67ae344ecb90
Ben-21 <bs@schaefer-inet.de>
2023-08-08 18:04:18 +0200

    adjust ProgressBar

--- frontend/src/components/ProgressBar.tsx
+++ frontend/src/components/ProgressBar.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
-import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
-import Typography from '@mui/material/Typography';
+import LinearProgress, {LinearProgressProps} from '@mui/material/LinearProgress';
 import Box from '@mui/material/Box';
 import {Project} from "../models/models.tsx";
+import styled from "@emotion/styled";
+import {useEffect} from "react";
 
 type Props = {
     project: Project;
@@ -11,43 +12,56 @@ type Props = {
 export default function ProgressBar(props: Props) {
     function LinearProgressWithLabel(props: LinearProgressProps & { value: number }) {
         return (
-            <Box sx={{display: 'flex', alignItems: 'center'}}>
-                <Box sx={{width: '80%', mr: 1}}>
-                    <LinearProgress variant="determinate" {...props} />
+            <>
+                <Box sx={{display: 'flex', alignItems: 'center'}}>
+                    <Box sx={{width: '100%', mr: 1}}>
+                        <LinearProgress variant="determinate" {...props} />
+                    </Box>
                 </Box>
-                <Box sx={{minWidth: 35}}>
-                    <Typography variant="body2" color="text.secondary">{`${Math.round(
-                        props.value,
-                    )}%`}</Typography>
-                </Box>
-            </Box>
+                <StyledPercentageDiv>
+                    Target achieved by {props.value}%
+                </StyledPercentageDiv>
+            </>
         );
+
+
     }
 
 
-        const [progress, setProgress] = React.useState(10);
+    const [progress, setProgress] = React.useState(10);
 
-        React.useEffect(() => {
-            const timer = setInterval(() => {
-                setProgress((prevProgress) => {
-                    if(prevProgress >= props.project.progress) {
-                        clearInterval(timer);
-                        return props.project.progress;
-                    }
-                    return prevProgress + 10;
-                });
-            }, 800);
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setProgress((prevProgress) => {
+                if (prevProgress >= props.project.progress) {
+                    clearInterval(timer);
+                    return props.project.progress;
+                }
+                return prevProgress + 10;
+            });
+        }, 800);
 
-            return () => {
-                clearInterval(timer);
-            };
-        }, [props.project.progress]);
+        return () => {
+            clearInterval(timer);
+        };
+    }, [props.project.progress]);
 
 
+    return (
+        <StyledBox>
+            {props.project.progress > 0 &&
+                <LinearProgressWithLabel color="success" sx={{height: 10, borderRadius: 5}} value={progress}/>}
+        </StyledBox>
+    );
+}
 
-        return (
-            <Box sx={{width: '100%'}}>
-                {props.project.progress > 0 && <LinearProgressWithLabel value={progress} />}
-            </Box>
-        );
-    }
+const StyledBox = styled.div`
+  margin: 10px 10% 0 12%;
+`;
+
+const StyledPercentageDiv = styled.div`
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  margin-top: 10px;
+`;
+
