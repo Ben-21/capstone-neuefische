d0628c82acea8329ff0ad36565b6b8a235b6d4d6
Ben-21 <bs@schaefer-inet.de>
2023-08-09 14:47:19 +0200

    add dependency and SecurityConfig

---
+++ backend/src/main/java/de/neuefische/capstone/backend/security/SecurityConfig.java
@@ -0,0 +1,60 @@
+package de.neuefische.capstone.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName(null);
+
+        return http.csrf(csrf -> csrf
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(requestHandler))
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .authorizeHttpRequests(httpRequests ->
+                        httpRequests
+                                .requestMatchers(HttpMethod.GET, "/api/projects").permitAll()
+                                .requestMatchers("/api/projects").authenticated()
+
+                                .requestMatchers(HttpMethod.GET, "/api/projects/**").permitAll()
+                                .requestMatchers("/api/projects/**").authenticated()
+                                .requestMatchers("/api/users/me").permitAll()
+                                .anyRequest().permitAll()
+                )
+                .logout(logout -> logout.logoutUrl("/api/users/logout")
+                        .deleteCookies("JSESSIONID"))
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return Argon2PasswordEncoder.defaultsForSpringSecurity_v5_8();
+    }
+}
+
+
+
+
+
+
+
+
+
