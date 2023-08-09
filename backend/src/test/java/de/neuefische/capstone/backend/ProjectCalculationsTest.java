516f8387943df72a8b2c00eb8008792ab0481531
Ben-21 <bs@schaefer-inet.de>
2023-08-09 12:53:10 +0200

    add test for calculateProgressForDonations

--- backend/src/test/java/de/neuefische/capstone/backend/ProjectCalculationsTest.java
+++ backend/src/test/java/de/neuefische/capstone/backend/ProjectCalculationsTest.java
@@ -3,17 +3,18 @@ package de.neuefische.capstone.backend;
 import de.neuefische.capstone.backend.models.*;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
- class ProjectCalculationsTest {
+class ProjectCalculationsTest {
 
     ProjectCalculations projectCalculations = new ProjectCalculations();
 
     @Test
-    void returnProjectWithProgress_whenCalculateProgress() {
+    void returnProjectWithProgress_whenCalculateProgressForVolunteer() {
         //Given
         Volunteer newVolunteer = new Volunteer(
                 "01A",
@@ -53,7 +54,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
         //Then
         assertEquals(expectedProject, actualProject);
+    }
 
+    @Test
+    void returnProjectWithProgress_whenCalculateProgressForDonation() {
+        //Given
+        Donation newDonation = new Donation(
+                "01A",
+                "01B",
+                "ProjectName",
+                "DonationName",
+                new BigDecimal(50)
+        );
 
+        Project project = new Project(
+                "01A",
+                "test",
+                "test",
+                Category.DONATION,
+                List.of(Demand.DONATIONINKIND),
+                0,
+                100,
+                "test",
+                List.of(newDonation),
+                new ArrayList<>()
+        );
+
+        Project expectedProject = new Project(
+                "01A",
+                "test",
+                "test",
+                Category.DONATION,
+                List.of(Demand.DONATIONINKIND),
+                50,
+                100,
+                "test",
+                List.of(newDonation),
+                new ArrayList<>()
+        );
+
+        //When
+        Project actualProject = projectCalculations.calculateProgressForDonations(project);
+
+        //Then
+        assertEquals(expectedProject, actualProject);
     }
 }
