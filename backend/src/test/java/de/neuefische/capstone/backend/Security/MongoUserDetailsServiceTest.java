64d1bb2eb4df7445808a1558d8683a3d833af646
Ben-21 <bs@schaefer-inet.de>
2023-08-10 15:51:37 +0200

    add unit tests for MongoUserDetailsService

---
+++ backend/src/test/java/de/neuefische/capstone/backend/Security/MongoUserDetailsServiceTest.java
@@ -0,0 +1,69 @@
+package de.neuefische.capstone.backend.Security;
+
+import de.neuefische.capstone.backend.security.MongoUser;
+import de.neuefische.capstone.backend.security.MongoUserDetailsService;
+import de.neuefische.capstone.backend.security.MongoUserRepository;
+import de.neuefische.capstone.backend.security.MongoUserWithNoId;
+import de.neuefische.capstone.backend.services.IdService;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class MongoUserDetailsServiceTest {
+
+    private final MongoUserRepository mongoUserRepository = mock(MongoUserRepository.class);
+    private final IdService idService = mock(IdService.class);
+    private final MongoUserDetailsService mongoUserDetailsService = new MongoUserDetailsService(mongoUserRepository, idService);
+
+    @Test
+    void returnUser_WhenUsernameExists() {
+        // Given
+        MongoUser mongoUser = new MongoUser("1", "test", "test");
+        when(mongoUserRepository.findByUsername("test"))
+                .thenReturn(Optional.of(mongoUser));
+
+        // When
+        UserDetails actual = mongoUserDetailsService.loadUserByUsername("test");
+
+        // Then
+        UserDetails expected = new User("test", "test", Collections.emptyList());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void throwException_WhenUsernameDoesNotExist() {
+        // Given
+        when(mongoUserRepository.findByUsername("test"))
+                .thenReturn(Optional.empty());
+
+        // When / Then
+        try {
+            mongoUserDetailsService.loadUserByUsername("test");
+        } catch (Exception e) {
+            assertEquals("Username test not found", e.getMessage());
+        }
+    }
+
+    @Test
+    void verifyRepoCalls_WhenUserIsRegistered() {
+        //Given
+        MongoUserWithNoId mongoUserWithNoId = new MongoUserWithNoId("test", "test");
+        when(mongoUserRepository.findByUsername("test"))
+                .thenReturn(Optional.empty());
+        when(idService.createRandomId())
+                .thenReturn("1");
+
+        //When
+       mongoUserDetailsService.registerUser(mongoUserWithNoId);
+
+        //Then
+        verify(mongoUserRepository).findByUsername("test");
+        verify(idService).createRandomId();
+    }
+}
