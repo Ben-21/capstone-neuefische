5b2c5546ce1f65a93d7d7a866268e3ec6346d981
Ben-21 <bs@schaefer-inet.de>
2023-08-18 11:42:30 +0200

    change max username chars

--- backend/src/main/java/de/neuefische/capstone/backend/security/MongoUserCreation.java
+++ backend/src/main/java/de/neuefische/capstone/backend/security/MongoUserCreation.java
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.Size;
 
 public record MongoUserCreation(
         @NotEmpty
-        @Size(min=4, max=14, message = "Username must be between 4 and 14 characters")
+        @Size(min=4, max=30, message = "Username must be between 4 and 14 characters")
         String username,
         @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9]).{6,20}$", message = "Invalid password")
         String password
