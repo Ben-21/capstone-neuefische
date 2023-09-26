41b2e0c9eb8e2649903200410a5cd17e28044118
Ben-21 <bs@schaefer-inet.de>
2023-09-26 16:42:18 +0200

    make cloudinary secure with api key, etc.

--- backend/src/main/java/de/neuefische/capstone/backend/imageupload/BeanConfigCloudinary.java
+++ backend/src/main/java/de/neuefische/capstone/backend/imageupload/BeanConfigCloudinary.java
@@ -4,10 +4,20 @@ import com.cloudinary.Cloudinary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Configuration
 public class BeanConfigCloudinary {
+    private static final String CLOUD_NAME = "dfzzbhu3x";
+    private static final String API_KEY = "399958539413299";
+    private static final String API_SECRET = "O4UId7cxw2-Hg5Z5ZwHXgms3-80";
     @Bean
     public Cloudinary createCloudinary() {
-        return new Cloudinary();
+        Map<String, String> config = new HashMap<>();
+        config.put("cloud_name",CLOUD_NAME);
+        config.put("api_key",API_KEY);
+        config.put("api_secret",API_SECRET);
+        return new Cloudinary(config);
     }
 }
--- backend/src/main/java/de/neuefische/capstone/backend/imageupload/CloudinaryService.java
+++ backend/src/main/java/de/neuefische/capstone/backend/imageupload/CloudinaryService.java
@@ -14,11 +14,7 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class CloudinaryService {
-    private final Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
-            "cloud_name", "dfzzbhu3x",
-            "api_key", "399958539413299",
-            "api_secret", "O4UId7cxw2-Hg5Z5ZwHXgms3-80"
-    ));
+    private final Cloudinary cloudinary;
     private static final String TEMP_DIRECTORY = System.getProperty("java.io.tmpdir");
 
     public String uploadImage(MultipartFile image) throws IOException {
