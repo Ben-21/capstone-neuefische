fd5b508a278ae2df9aefed9cb9c6608b01b45648
Ben-21 <bs@schaefer-inet.de>
2023-09-26 16:12:29 +0200

    make cloudinary secure with api key, etc.

--- backend/src/main/java/de/neuefische/capstone/backend/imageupload/CloudinaryService.java
+++ backend/src/main/java/de/neuefische/capstone/backend/imageupload/CloudinaryService.java
@@ -1,6 +1,7 @@
 package de.neuefische.capstone.backend.imageupload;
 
 import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,7 +14,11 @@ import java.util.Map;
 @Service
 @RequiredArgsConstructor
 public class CloudinaryService {
-    private final Cloudinary cloudinary;
+    private final Cloudinary cloudinary = new Cloudinary(ObjectUtils.asMap(
+            "cloud_name", "dfzzbhu3x",
+            "api_key", "399958539413299",
+            "api_secret", "O4UId7cxw2-Hg5Z5ZwHXgms3-80"
+    ));
     private static final String TEMP_DIRECTORY = System.getProperty("java.io.tmpdir");
 
     public String uploadImage(MultipartFile image) throws IOException {
@@ -22,6 +27,9 @@ public class CloudinaryService {
 
         Map response = cloudinary.uploader().upload(fileToUpload, Collections.emptyMap());
 
-        return response.get("url").toString();
+        String httpUrl = response.get("url").toString();
+        String httpsUrl = httpUrl.replace("http://", "https://");
+
+        return httpsUrl;
     }
 }
